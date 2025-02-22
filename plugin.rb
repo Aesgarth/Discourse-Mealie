29896d5482d875e9d32165ee6b7afe9a4e2a07bf
Aesgarth <getflair@gmail.com>
2025-02-22 11:41:24 +0000

    this is stupid

--- plugin.rb
+++ plugin.rb
@@ -6,7 +6,9 @@
 
 enabled_site_setting :discourse_mealie_enabled
 
+require_dependency "application_controller"
 require_relative "app/controllers/mealie_controller"
+require_dependency "application_controller"
 require_relative "app/controllers/admin_mealie_controller"
 
 after_initialize do
